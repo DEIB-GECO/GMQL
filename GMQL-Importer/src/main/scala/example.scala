6439b2c86b39ba6d5640f8f3bfa920673af8c7ca
nachodox <nachodox@gmail.com>
2016-11-07 15:55:29 +0100

    standardize enablers for transformation/download/load. code cleaning

--- GMQL-Importer/src/main/scala/example.scala
+++ GMQL-Importer/src/main/scala/example.scala
@@ -1,12 +1,66 @@
-import it.polimi.genomics.importer.GMQLImporter.{GMQLDataset, GMQLLoader, GMQLSource, GMQLDownloader, GMQLTransformer}
+import it.polimi.genomics.importer.GMQLImporter.{GMQLDataset, GMQLDownloader, GMQLLoader, GMQLSource, GMQLTransformer}
 import it.polimi.genomics.importer.GMQLImporter.utils.SCHEMA_LOCATION
-import scala.xml.XML
+
+import scala.xml.{Elem, XML}
+import java.io.File
 import org.slf4j._
 object example {
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   def main(args: Array[String]): Unit = {
-    runDemo()
+    runTest("GMQL-Importer/Example/xml/ConfigurationTCGA2BED.xml")
+  }
+
+  def runTest(xmlConfigPath: String): Unit = {
+    //general settings
+    val file: Elem = XML.loadFile(xmlConfigPath)
+    val outputFolder = (file \\ "settings" \ "output_folder").text
+    val downloadEnabled = if ("true".equalsIgnoreCase((file \\ "settings" \ "download_enabled").text)) true else false
+    val transformEnabled = if ("true".equalsIgnoreCase((file \\ "settings" \ "transform_enabled").text)) true else false
+    val loadEnabled = if ("true".equalsIgnoreCase((file \\ "settings" \ "load_enabled").text)) true else false
+    //load sources
+    val sources = (file \\ "source_list" \ "source").map(source => {
+      GMQLSource(
+        (source \ "@name").text,
+        (source \ "url").text,
+        outputFolder+File.separator+(source \ "output_folder").text,
+        (source \ "gmql_user").text,
+        (source \ "downloader").text,
+        (source \ "transformer").text,
+        if ((source \ "download_enabled").text.toLowerCase == "true") true else false,
+        if ((source \ "transform_enabled").text.toLowerCase == "true") true else false,
+        if ((source \ "load_enabled").text.toLowerCase == "true") true else false,
+        (source \ "parameter_list" \ "parameter").map(parameter => {
+          ((parameter \ "key").text, (parameter \ "value").text)
+        }),
+        (source \ "dataset_list" \ "dataset").map(dataset => {
+          GMQLDataset(
+            (dataset \ "@name").text,
+            (dataset \ "output_folder").text,
+            (dataset \ "schema").text,
+            SCHEMA_LOCATION.withName((dataset \ "schema" \ "@location").text),
+            if ((dataset \ "download_enabled").text.toLowerCase == "true") true else false,
+            if ((dataset \ "transform_enabled").text.toLowerCase == "true") true else false,
+            if ((dataset \ "load_enabled").text.toLowerCase == "true") true else false,
+            (dataset \ "parameter_list" \ "parameter").map(parameter => {
+              ((parameter \ "key").text, (parameter \ "value").text)
+            })
+          )
+        })
+      )
+    })
+    //start DTL
+    sources.foreach(source => {
+      if (downloadEnabled && source.downloadEnabled) {
+        Class.forName(source.downloader).newInstance.asInstanceOf[GMQLDownloader].download(source)
+      }
+      if (transformEnabled && source.transformEnabled) {
+        Class.forName(source.transformer).newInstance.asInstanceOf[GMQLTransformer].transform(source)
+      }
+      if (loadEnabled && source.loadEnabled) {
+        GMQLLoader.loadIntoGMQL(source)
+      }
+    })
   }
   /**
     * Starts a demonstration of the functionality of GMQL-Importer,
@@ -21,8 +75,8 @@ object example {
         (source \ "output_folder").text,
         (source \ "gmql_user").text,
         (source \ "downloader").text,
-        if ((source \ "download_enabled").text.toLowerCase == "true") true else false,
         (source \ "transformer").text,
+        if ((source \ "download_enabled").text.toLowerCase == "true") true else false,
         if ((source \ "transform_enabled").text.toLowerCase == "true") true else false,
         if ((source \ "load_enabled").text.toLowerCase == "true") true else false,
         (source \ "parameter_list" \ "parameter").map(parameter => {
@@ -30,9 +84,13 @@ object example {
         }),
         (source \ "dataset_list" \ "dataset").map(dataset => {
           GMQLDataset(
+            (dataset \ "@name").text,
             (dataset \ "output_folder").text,
             (dataset \ "schema").text,
             SCHEMA_LOCATION.withName((dataset \ "schema" \ "@location").text),
+            if ((dataset \ "download_enabled").text.toLowerCase == "true") true else false,
+            if ((dataset \ "transform_enabled").text.toLowerCase == "true") true else false,
+            if ((dataset \ "load_enabled").text.toLowerCase == "true") true else false,
             (dataset \ "parameter_list" \ "parameter").map(parameter => {
               ((parameter \ "key").text, (parameter \ "value").text)
             })
@@ -41,13 +99,13 @@ object example {
       )
     })
     sources.foreach(source =>{
-      if(source.download_enabled){
+      if(source.downloadEnabled){
         Class.forName(source.downloader).newInstance.asInstanceOf[GMQLDownloader].download(source)
       }
-      if(source.transform_enabled){
+      if(source.transformEnabled){
         Class.forName(source.transformer).newInstance.asInstanceOf[GMQLTransformer].transform(source)
       }
-      if(source.load_enabled){
+      if(source.loadEnabled){
         GMQLLoader.loadIntoGMQL(source)
       }
     })
