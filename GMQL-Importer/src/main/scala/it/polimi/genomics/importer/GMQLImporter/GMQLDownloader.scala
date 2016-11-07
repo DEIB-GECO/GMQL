e80cf1bcff2916f2c631c2200e6ae0bd89a09927
nachodox <nachodox@gmail.com>
2016-11-07 14:16:54 +0100

    description modified

--- GMQL-Importer/src/main/scala/it/polimi/genomics/importer/GMQLImporter/GMQLDownloader.scala
+++ GMQL-Importer/src/main/scala/it/polimi/genomics/importer/GMQLImporter/GMQLDownloader.scala
@@ -10,6 +10,10 @@ trait GMQLDownloader {
   /**
     * downloads the files from the source defined in the loader
     * into the folder defined in the loader
+    *
+    * For each dataset, download method should put the downloaded files inside
+    * /source.outputFolder/dataset.outputFolder/Downloads
+    *
     * @param source contains specific download and sorting info.
     */
   def download(source: GMQLSource):Unit
