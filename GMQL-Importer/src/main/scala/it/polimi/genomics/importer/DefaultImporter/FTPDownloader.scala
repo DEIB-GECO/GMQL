7f673df1421642b3babd884818b6460a77717e5d
nachodox <nachodox@gmail.com>
2016-11-10 19:28:00 +0100

    temporary fix for timeout

--- GMQL-Importer/src/main/scala/it/polimi/genomics/importer/DefaultImporter/FTPDownloader.scala
+++ GMQL-Importer/src/main/scala/it/polimi/genomics/importer/DefaultImporter/FTPDownloader.scala
@@ -22,7 +22,7 @@ class FTPDownloader extends GMQLDownloader {
     */
   override def download(source: GMQLSource): Unit = {
     if(source.downloadEnabled) {
-      logger.info("Starting download for: " + source.url)
+      logger.info("Starting download for: " + source.name)
       if (!new java.io.File(source.outputFolder).exists) {
         new java.io.File(source.outputFolder).mkdirs()
       }
@@ -48,8 +48,9 @@ class FTPDownloader extends GMQLDownloader {
         source.parameters.filter(_._1 == "password").head._2).getOrElse(false)) {
 
         logger.info("Connected to ftp: " + source.url)
-        recursiveDownload(ftp, source)
+        val workingDirectory = ftp.workingDirectory()
         ftp.disconnect()
+        recursiveDownload(workingDirectory, source)
 
         source.datasets.foreach(dataset => {
           if (dataset.downloadEnabled) {
@@ -61,21 +62,19 @@ class FTPDownloader extends GMQLDownloader {
         })
       }
       else
-        logger.error("ftp connection with " + source.url + " couldn't be handled." +
-          "username:" + source.parameters.filter(_._1 == "username").head._2 +
-          "password:" + source.parameters.filter(_._1 == "password").head._2)
+        logger.warn("ftp connection with " + source.url + " couldn't be handled.")
     }
   }
 
   /**
     * recursively checks all folders and subfolders matching with the regular expressions defined in the information
     *
-    * @param ftp    current state of the ftp connection
+    * @param workingDirectory    current folder of the ftp connection
     * @param source configuration for the downloader, folders for input and output by regex and also for files.
     */
-  private def recursiveDownload(ftp: FTP, source: GMQLSource): Unit = {
-    checkFolderForDownloads(ftp, source)
-    downloadSubfolders(ftp, source)
+  private def recursiveDownload(workingDirectory: String, source: GMQLSource): Unit = {
+    checkFolderForDownloads(workingDirectory, source)
+    downloadSubfolders(workingDirectory, source)
   }
 
   /**
@@ -83,68 +82,89 @@ class FTPDownloader extends GMQLDownloader {
     * puts all content into information.outputFolder/dataset.outputFolder/Downloads/
     * (this is because TCGA2BED does no transform and we dont want just to copy the files).
     *
-    * @param ftp    current state of the ftp connection
+    * @param workingDirectory    current state of the ftp connection
     * @param source configuration for downloader, folders for input and output by regex and also for files
     */
-  private def checkFolderForDownloads(ftp: FTP, source: GMQLSource): Unit = {
-    for (dataset <- source.datasets) {
-      if(dataset.downloadEnabled) {
-        if (ftp.workingDirectory().matches(dataset.parameters.filter(_._1 == "folder_regex").head._2)) {
-          val outputPath = source.outputFolder + File.separator + dataset.outputFolder + File.separator + "Downloads"
-          val log = new FileLogger(outputPath)
+  private def checkFolderForDownloads(workingDirectory: String, source: GMQLSource): Unit = {
+    val ftp = new FTP()
+    if (ftp.connectWithAuth(
+      source.url,
+      source.parameters.filter(_._1 == "username").head._2,
+      source.parameters.filter(_._1 == "password").head._2).getOrElse(false)) {
+      ftp.cd(workingDirectory)
+      for (dataset <- source.datasets) {
+        if (dataset.downloadEnabled) {
+          if (ftp.workingDirectory().matches(dataset.parameters.filter(_._1 == "folder_regex").head._2)) {
+            val outputPath = source.outputFolder + File.separator + dataset.outputFolder + File.separator + "Downloads"
+            val log = new FileLogger(outputPath)
 
-          logger.info("Searching: " + ftp.workingDirectory())
-          if (!new java.io.File(outputPath).exists) {
-            new java.io.File(outputPath).mkdirs()
-          }
+            logger.info("Searching: " + ftp.workingDirectory())
+            if (!new java.io.File(outputPath).exists) {
+              new java.io.File(outputPath).mkdirs()
+            }
 
-          val files = ftp.listFiles().filter(_.isFile).filter(_.getName.matches(
-            dataset.parameters.filter(_._1 == "files_regex").head._2))
+            val files = ftp.listFiles().filter(_.isFile).filter(_.getName.matches(
+              dataset.parameters.filter(_._1 == "files_regex").head._2))
 
-          for (file <- files) {
-            if (log.checkIfUpdate(
-              file.getName,
-              ftp.workingDirectory() + File.separator + file.getName,
-              file.getSize.toString,
-              file.getTimestamp.getTime.toString)) {
-              logger.debug("Starting download of: " + ftp.workingDirectory() + File.separator + file.getName)
-              var downloaded = ftp.downloadFile(file.getName, outputPath + File.separator + file.getName)
-              var timesTried = 0
-              while (!downloaded && timesTried < 4) {
-                downloaded = ftp.downloadFile(file.getName, outputPath + File.separator + file.getName)
-                timesTried += 1
-              }
-              if (!downloaded) {
-                logger.error("Downloading: " + ftp.workingDirectory() + File.separator + file.getName + " FAILED")
-                log.markAsFailed(file.getName)
-              }
-              else {
-                logger.info("Downloading: " + ftp.workingDirectory() + File.separator + file.getName + " DONE")
-                log.markAsUpdated(file.getName)
+            for (file <- files) {
+              if (log.checkIfUpdate(
+                file.getName,
+                ftp.workingDirectory() + File.separator + file.getName,
+                file.getSize.toString,
+                file.getTimestamp.getTime.toString)) {
+                logger.debug("Starting download of: " + ftp.workingDirectory() + File.separator + file.getName)
+                var downloaded = true//ftp.downloadFile(file.getName, outputPath + File.separator + file.getName)
+                var timesTried = 0
+                while (!downloaded && timesTried < 4) {
+                  downloaded = ftp.downloadFile(file.getName, outputPath + File.separator + file.getName)
+                  timesTried += 1
+                }
+                if (!downloaded) {
+                  logger.error("Downloading: " + ftp.workingDirectory() + File.separator + file.getName + " FAILED")
+                  log.markAsFailed(file.getName)
+                }
+                else {
+                  logger.info("Downloading: " + ftp.workingDirectory() + File.separator + file.getName + " DONE")
+                  log.markAsUpdated(file.getName)
+                }
               }
             }
+            log.saveTable()
           }
-          log.saveTable()
         }
       }
     }
+    else
+      logger.error("connection lost with FTP, skipping "+workingDirectory)
   }
 
   /**
     * Finds all subfolders in the working directory and performs checkFolderForDownloads on it
     *
-    * @param ftp    current state of the ftp connection
+    * @param workingDirectory    current folder of the ftp connection
     * @param source configuration for downloader, folders for input and output by regex and also for files
     */
-  private def downloadSubfolders(ftp: FTP, source: GMQLSource): Unit = {
-    ftp.listDirectories().foreach(directory => {
-      ftp.cd(directory.getName)
-      recursiveDownload(ftp, source)
-      ftp.cd("..")
-    })
+  private def downloadSubfolders(workingDirectory: String, source: GMQLSource): Unit = {
+
+    val ftp = new FTP()
+    if (ftp.connectWithAuth(
+      source.url,
+      source.parameters.filter(_._1 == "username").head._2,
+      source.parameters.filter(_._1 == "password").head._2).getOrElse(false)) {
+      ftp.cd(workingDirectory)
+      val directories = ftp.listDirectories()
+      ftp.disconnect()
+      directories.foreach(directory => {
+        recursiveDownload(workingDirectory+File.separator+directory.getName, source)
+      })
+    }
+    else
+      logger.error("connection lost with FTP, skipped "+workingDirectory)
   }
 
   /**
+    * deprecated
+    *
     * this method is meant to be used for downloading ftp schema
     * puts the schema in a folder called "downloadedSchema" with chosen name
     *
