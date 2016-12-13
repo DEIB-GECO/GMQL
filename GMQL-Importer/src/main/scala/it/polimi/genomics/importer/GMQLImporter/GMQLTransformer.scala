d0758094197ef04b1f8e0e06a71a5b04f50bdddd
nachodox <nachodox@gmail.com>
2016-12-13 18:52:36 +0100

    modified the trait to implement "integrator"

--- GMQL-Importer/src/main/scala/it/polimi/genomics/importer/GMQLImporter/GMQLTransformer.scala
+++ GMQL-Importer/src/main/scala/it/polimi/genomics/importer/GMQLImporter/GMQLTransformer.scala
@@ -8,10 +8,23 @@ package it.polimi.genomics.importer.GMQLImporter
   * ex: TCGA2BEDSorter should use TCGA2BEDSorter
   */
 trait GMQLTransformer {
+
   /**
-    * using the information in the loader should convert the downloaded files
-    * into data and metadata as specified in GDM
-    * @param source contains specific download and sorting info.
+    * recieves .json and .bed.gz files and transform them to get metadata in .meta files and region in .bed files.
+    * @param source source where the files belong to.
+    * @param originPath path for the  "Downloads" folder
+    * @param destinationPath path for the "Transformations" folder
+    * @param originalFilename name of the original file .json/.gz
+    * @param filename name of the new file .meta/.bed
+    * @return List(fileId, filename) for the transformed files.
     */
-  def transform(source: GMQLSource):Unit
+  def transform(source: GMQLSource,originPath: String, destinationPath: String, originalFilename:String,
+                filename: String):Unit
+
+  /**
+    * by receiving an original filename returns the new GDM candidate name.
+    * @param filename original filename
+    * @return candidate names for the files derived from the original filename.
+    */
+  def getCandidateNames(filename: String): List[String]
 }
