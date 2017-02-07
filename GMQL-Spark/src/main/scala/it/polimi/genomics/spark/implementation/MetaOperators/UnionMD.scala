cc04eb656ec6e010db00efa3eed630c516652aff
Olga Gorlova <Pers_Life>
2017-02-07 17:44:11 +0100

    Fixed: avoid provenance prefixes on metadata attributes in UNION.

--- GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/MetaOperators/UnionMD.scala
+++ GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/MetaOperators/UnionMD.scala
@@ -32,12 +32,12 @@ object UnionMD {
     //change ID of each region according to previous computation
     val leftMod  =
       left.map((m) => {
-        (Hashing.md5.newHasher.putLong(0L).putLong(m._1).hash.asLong, (leftTag +"."+m._2._1,m._2._2))
+        (Hashing.md5.newHasher.putLong(0L).putLong(m._1).hash.asLong, (m._2._1,m._2._2))
       })
 
     val rightMod  =
       right.map((m) => {
-        (Hashing.md5.newHasher.putLong(1L).putLong( m._1).hash.asLong, (rightTag+"."+m._2._1,m._2._2))
+        (Hashing.md5.newHasher.putLong(1L).putLong( m._1).hash.asLong, (m._2._1,m._2._2))
       })
 
     //merge datasets
