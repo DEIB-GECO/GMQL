47147642b3bf2920bf00dd820f634a373bc731bd
akaitoua <abdulrahman.kaitoua@polimi.it>
2017-02-15 13:44:09 +0100

    Update GMQLSparkExecutor.scala

--- GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/GMQLSparkExecutor.scala
+++ GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/GMQLSparkExecutor.scala
@@ -145,37 +145,46 @@ class GMQLSparkExecutor(val binSize : BinSize = BinSize(), val maxBinDistance : 
 
           val outSample = "S"
 
-          val Ids = metaRDD.keys.distinct()
+                   val Ids = metaRDD.keys.distinct()
+          val newIDS: Map[Long, Long] = Ids.zipWithIndex().collectAsMap()
+          val newIDSbroad = sc.broadcast(newIDS)
+
           val regionsPartitioner = new HashPartitioner(Ids.count.toInt)
 
           val keyedRDD = if(!(outputFormat == GMQLOutputFormat.GTF)){
-             regionRDD.partitionBy(regionsPartitioner).map(x => (outSample+"_"+x._1._1.toString+".gdm", x._1._2 + "\t" + x._1._3 + "\t" + x._1._4 + "\t" + x._1._5 + "\t" + x._2.mkString("\t"))).partitionBy(regionsPartitioner)
-          }else{
+             regionRDD.map(x => (outSample+"_"+ "%05d".format(newIDSbroad.value.get(x._1._1).get)+".gdm",
+               x._1._2 + "\t" + x._1._3 + "\t" + x._1._4 + "\t" + x._1._5 + "\t" + x._2.mkString("\t")))
+               .partitionBy(regionsPartitioner).mapPartitions(x=>x.toList.sortBy{s=> val data = s._2.split("\t"); (data(0),data(1).toLong,data(2).toLong)}.iterator)
+          }else {
             val jobname = outputFolderName
-            val score= variable.schema.zipWithIndex.filter(x=>x._1._1.toLowerCase().equals("score"))
-            val scoreIndex = if(score.size>0) score(0)._2 else -1
-            regionRDD.map{x =>
-              val values = variable.schema.zip(x._2).flatMap{s=>if(s._1._1.equals("score")) None else Some(s._1._1+" \""+s._2+"\";")}.mkString(" ")
-              (outSample+"_"+x._1._1.toString+".gtf",
-                x._1._2                                                                  //chrom
-//                  + "\t" + jobname.substring(jobname.lastIndexOf("_")+1,jobname.length)   //variable name
-                  + "\t" + "GMQL"   //variable name
-                  + "\t" +"Region"
-                  + "\t" +x._1._3 + "\t" + x._1._4 + "\t"                                 //start , stop
-                  + {if(scoreIndex>=0) x._2(scoreIndex) else "0.0"}                       //score
-                  +"\t" + (if(x._1._5.equals('*')) '.' else x._1._5)  + "\t"                                                  //strand
-                  +"."                                                                  //frame
+            val score = variable.schema.zipWithIndex.filter(x => x._1._1.toLowerCase().equals("score"))
+            val scoreIndex = if (score.size > 0) score(0)._2 else -1
+            regionRDD.map { x =>
+              val values = variable.schema.zip(x._2).flatMap { s => if (s._1._1.equals("score")) None else Some(s._1._1 + " \"" + s._2 + "\";") }.mkString(" ")
+              (outSample + "_" + "%05d".format(newIDSbroad.value.get(x._1._1).get) + ".gtf",
+                x._1._2 //chrom
+                  //                  + "\t" + jobname.substring(jobname.lastIndexOf("_")+1,jobname.length)   //variable name
+                  + "\t" + "GMQL" //variable name
+                  + "\t" + "Region"
+                  + "\t" + x._1._3 + "\t" + x._1._4 + "\t" //start , stop
+                  + {
+                  if (scoreIndex >= 0) x._2(scoreIndex) else "0.0"
+                } //score
+                  + "\t" + (if (x._1._5.equals('*')) '.' else x._1._5) + "\t" //strand
+                  + "." //frame
                   + "\t" + values
-                )
+              )
             }.partitionBy(regionsPartitioner)
+              .mapPartitions(x=>x.toList.sortBy{s=> val data = s._2.split("\t"); (data(0),data(3).toLong,data(4).toLong)}.iterator)
           }
 
           writeMultiOutputFiles.saveAsMultipleTextFiles(keyedRDD, RegionOutputPath)
 
+
           val metaKeyValue = if(!(outputFormat == GMQLOutputFormat.GTF)){
-            metaRDD.map(x => (outSample+"_"+x._1.toString + ".gdm.meta", x._2._1 + "\t" + x._2._2)).repartition(1).sortBy(x=>(x._1,x._2))
+            metaRDD.map(x => (outSample+"_"+ "%05d".format(newIDSbroad.value.get(x._1).get) + ".gdm.meta", x._2._1 + "\t" + x._2._2)).repartition(1).sortBy(x=>(x._1,x._2))
           }else{
-            metaRDD.map(x => (outSample+"_"+x._1.toString + ".gtf.meta", x._2._1 + "\t" + x._2._2)).repartition(1).sortBy(x=>(x._1,x._2))
+            metaRDD.map(x => (outSample+"_"+ "%05d".format(newIDSbroad.value.get(x._1)) + ".gtf.meta", x._2._1 + "\t" + x._2._2)).repartition(1).sortBy(x=>(x._1,x._2))
           }
           writeMultiOutputFiles.saveAsMultipleTextFiles(metaKeyValue, MetaOutputPath)
 
