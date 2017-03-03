dc619de046b7fe59b5d502a5a99a6952d8e2a9c7
Olga Gorlova <Pers_Life>
2017-03-03 13:50:24 +0100

    Added use of EXACT and FULLNAME in semijoin

--- GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/MetaOperators/SemiJoinMD.scala
+++ GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/MetaOperators/SemiJoinMD.scala
@@ -1,6 +1,6 @@
 package it.polimi.genomics.spark.implementation.MetaOperators
 
-import it.polimi.genomics.core.DataStructures.MetaJoinCondition.MetaJoinCondition
+import it.polimi.genomics.core.DataStructures.MetaJoinCondition.{Default, Exact, FullName, MetaJoinCondition}
 import it.polimi.genomics.core.DataStructures.MetaOperator
 import it.polimi.genomics.core.DataTypes.MetaType
 import it.polimi.genomics.core.exception.SelectFormatException
@@ -21,19 +21,37 @@ object SemiJoinMD {
 
     logger.info("----------------SemiJoinMD executing..")
 
-    val input = executor.implement_md(inputDataset, sc).map(x=>(x._2,x._1))
+    val input = executor.implement_md(inputDataset, sc)/*.map(x=>(x._2,x._1))*/
     val validInputId =
       executor
         .implement_md(externalMeta, sc)
-        .filter(a => joinCondition.attributes.foldLeft(false)( (r,c) => r | a._2._1.endsWith(c.toString())) )
-        .map(x=>(x._2,x._1))
-        .join(input)
+        .filter(a => joinCondition.attributes.foldLeft(false)( (r,c) => r | /*a._2._1.endsWith(c.toString())*/
+            {
+              if (c.isInstanceOf[FullName]) (a._2._1.equals(c.asInstanceOf[FullName].attribute)||a._2._1.endsWith("."+c.asInstanceOf[FullName].attribute))
+              else if (c.isInstanceOf[Exact]) (a._2._1.equals(c.asInstanceOf[Exact].attribute))
+              else (a._2._1.equals(c.asInstanceOf[Default].attribute)||a._2._1.endsWith("."+c.asInstanceOf[Default].attribute))
+            }))
+        .map(x=>/*(x._2,x._1)*/
+          joinCondition.attributes.flatMap { att =>
+            if (att.isInstanceOf[FullName] || att.isInstanceOf[Exact]) Some(((x._2._1, x._2._2), x._1))
+            else if (x._2._1.equals(att.asInstanceOf[Default].attribute) || x._2._1.endsWith("." + att.asInstanceOf[Default].attribute))
+              Some(((att.asInstanceOf[Default].attribute, x._2._2), x._1))
+            else Some(((x._2._1, x._2._2), x._1))
+          }.head)
+        .join(/*input*/
+          input.map(x=>
+            joinCondition.attributes.flatMap { att =>
+              if (att.isInstanceOf[FullName] || att.isInstanceOf[Exact]) Some(((x._2._1, x._2._2), x._1))
+              else if (x._2._1.equals(att.asInstanceOf[Default].attribute) || x._2._1.endsWith("." + att.asInstanceOf[Default].attribute))
+                Some(((att.asInstanceOf[Default].attribute.toString(), x._2._2), x._1))
+              else Some(((x._2._1, x._2._2), x._1))
+            }.head))
         .map(a => ((a._2._1, a._2._2), (a._1._1, 1)))
         .distinct()
         .reduceByKey((a , b ) => ( a._1, a._2+b._2))
         .filter(_._2._2 >= (joinCondition.attributes.size))
         .map(_._1._2).distinct()
         .collect
-    input.filter(a => validInputId.contains(a._2)).map(x=>(x._2,x._1))
+    input.map(x=>(x._2,x._1)).filter(a => validInputId.contains(a._2)).map(x=>(x._2,x._1))
   }
 }
