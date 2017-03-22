d34233acc8e83085e5408201105ff39ffb790ed6
akaitoua <abdulrahman.kaitoua@polimi.it>
2017-03-22 17:38:29 +0100

    Cover

--- GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/RegionsOperators/GenometricCover/GenometricCover.scala
+++ GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/RegionsOperators/GenometricCover/GenometricCover.scala
@@ -6,7 +6,6 @@ import it.polimi.genomics.core.DataStructures.RegionAggregate.RegionsToRegion
 import it.polimi.genomics.core.DataStructures.{MetaGroupOperator, RegionOperator}
 import it.polimi.genomics.core.DataTypes._
 import it.polimi.genomics.core.{GDouble, GValue}
-import it.polimi.genomics.core.GRecordKey
 import it.polimi.genomics.core.exception.SelectFormatException
 import it.polimi.genomics.spark.implementation.GMQLSparkExecutor
 import it.polimi.genomics.spark.implementation.RegionsOperators.GenometricMap.{ GMAP4}
@@ -33,12 +32,15 @@ object GenometricCover {
     // CREATE DATASET RECURSIVELY
     val ds: RDD[GRECORD] = executor.implement_rd(inputDataset, sc)
 
-    val groups: Broadcast[Map[Long, Long]] = if(grouping.isDefined)
-      sc.broadcast(executor.implement_mgd(grouping.get, sc).collectAsMap())
-    else sc.broadcast(Map(0l->0l))
+    val groups: Option[Broadcast[Map[Long, Long]]] = if(grouping.isDefined)
+      Some(sc.broadcast(executor.implement_mgd(grouping.get, sc).collectAsMap()))
+    else None
 
-    val groupIds = groups.value.toList.map(_._2).distinct
-
+    val groupIds = if(groups.isDefined){
+      groups.get.value.toList.map(_._2).distinct
+    } else {
+      List(0L)
+    }
 
     //ASSGIN GROUPS TO THE DATA AND IF NOT GROUPED GIVE ID 0 AS THE GROUP ID
     val groupedDs : RDD[(Long, String, Long, Long, Char, Long, Array[GValue])] =
@@ -54,9 +56,10 @@ object GenometricCover {
     // calculate the value only if needed
     val allValue : Map[Long, Int] =
     if(min.isInstanceOf[ALL] || max.isInstanceOf[ALL]){
-      if(grouping.isDefined){
+      if(groups.isDefined){
         // SWITCH (sampleID, GroupID) to (GroupID,SampleID) THEN GROUP BY GROUPID AND COUNT THE SAMPLES IN EACH GROUP
-        groups.value.groupBy(_._2).map(x=>(x._1,x._2.size))
+        groups.get.value.groupBy(_._2).map(x=>(x._1,x._2.size))
+
       }else{
         val samplesCount = groupedDs.map(x=>x._6).distinct().count.toInt
         groupIds.map(v => ((v, samplesCount))).foldRight(new HashMap[Long, Int])((a,z) => z + a)
@@ -80,22 +83,21 @@ object GenometricCover {
       }
 
     // EXECUTE COVER ON BINS
-      val ss = extracted
-        // collapse coincident point
-        .reduceByKey((a,b) => {
-          ( a.merged(b)({case ((k,v1),(_,v2)) => (k,v1+v2)}))
-        })
+    val ss = extracted
+      // collapse coincident point
+      .reduceByKey((a,b) => {
+      ( a.merged(b)({case ((k,v1),(_,v2)) => (k,v1+v2)}))
+    })
 
     val binnedPureCover : RDD[Grecord] =ss.flatMap(bin => {
-          val points : List[(Int, Int)] = bin._2.toList.sortBy(_._1)
-          coverFlag match {
-            case CoverFlag.COVER => coverHelper( points.iterator,  minimum, maximum , bin._1._3, bin._1._1, bin._1._4, bin._1._2, binSize)
-            case CoverFlag.HISTOGRAM => histogramHelper(points.iterator,minimum, maximum, bin._1._3, bin._1._1, bin._1._4, bin._1._2, binSize)
-            case CoverFlag.SUMMIT => summitHelper(points.iterator,minimum, maximum,  bin._1._3, bin._1._1, bin._1._4, bin._1._2, binSize)
-            case CoverFlag.FLAT => coverHelper(  points.iterator, minimum, maximum, bin._1._3, bin._1._1, bin._1._4, bin._1._2, binSize)
-          }
-        })
-
+      val points : List[(Int, Int)] = bin._2.toList.sortBy(_._1)
+      coverFlag match {
+        case CoverFlag.COVER => coverHelper( points.iterator,  minimum, maximum , bin._1._3, bin._1._1, bin._1._4, bin._1._2, binSize)
+        case CoverFlag.HISTOGRAM => histogramHelper(points.iterator,minimum, maximum, bin._1._3, bin._1._1, bin._1._4, bin._1._2, binSize)
+        case CoverFlag.SUMMIT => summitHelper(points.iterator,minimum, maximum,  bin._1._3, bin._1._1, bin._1._4, bin._1._2, binSize)
+        case CoverFlag.FLAT => coverHelper(  points.iterator, minimum, maximum, bin._1._3, bin._1._1, bin._1._4, bin._1._2, binSize)
+      }
+    })
 
     //SPLIT DATASET -> FIND REGIONS THAT MAY CROSS A BIN
     val valid: RDD[Grecord] =
@@ -108,28 +110,25 @@ object GenometricCover {
     // HAVE COINCIDENT STOP AND START
     // HAVE SAME INTERSECTION COUNTER
     val joined : RDD[Grecord] =
-      notValid
-        .groupBy(x=>(x._1,x._2,x._5))
-        .flatMap{x=>
-          val i = x._2.toList.sortBy(x=>(x._3,x._4)).iterator
-          var out = List[Grecord]()
-          if (i.hasNext) {
-            var old: Grecord = i.next()
-            while (i.hasNext) {
-              val current = i.next()
-              val oldCount = old._6(0).asInstanceOf[GDouble].v
-              val newCount = current._6(0).asInstanceOf[GDouble].v
-
-              if (old._4.equals(current._3) && (!coverFlag.equals(CoverFlag.HISTOGRAM) || oldCount.equals(newCount))) {
-                old = (old._1, old._2, old._3, current._4, old._5, Array[GValue]():+GDouble(Math.max(oldCount,newCount)))
-              } else {
-                out = out :+ old
-                old = current
-              }
+    notValid
+      .groupBy(x=>(x._1,x._2,x._5))
+      .flatMap{x=>
+        val i = x._2.toList.sortBy(x=>(x._3,x._4)).iterator
+        var out = List[Grecord]()
+        if (i.hasNext) {
+          var old: Grecord = i.next()
+          while (i.hasNext) {
+            val current = i.next()
+            if (old._4.equals(current._3) && (!coverFlag.equals(CoverFlag.HISTOGRAM) || old._6(0).asInstanceOf[GDouble].v.equals(current._6(0).asInstanceOf[GDouble].v))) {
+              old = (old._1, old._2, old._3, current._4, old._5, Array[GValue]():+GDouble(Math.max(old._6(0).asInstanceOf[GDouble].v,current._6(0).asInstanceOf[GDouble].v)))
+            } else {
+              out = out :+ old
+              old = current
             }
-           out = out :+ old
           }
-          out
+          out = out :+ old
+        }
+        out
       }
 
     val pureCover : RDD[Grecord] = valid.union(joined)
@@ -144,28 +143,24 @@ object GenometricCover {
 
   /**
     *
-    * @param basesList
+    * @param start
+    * @param count
+    * @param countMax
+    * @param recording
     * @param minimum
     * @param maximum
-    * @param id
-    * @param chr
-    * @param strand
-    * @param bin
-    * @param binSize
-    * @return
+    * @param i
     */
-  final def coverHelper(basesList : Iterator[(Int, Int)], minimum : Map[Long,Int], maximum : Map[Long,Int],
-                        id : Long, chr : String, strand : Char, bin : Int, binSize : Long) : List[Grecord] = {
+  final def coverHelper( si : Iterator[(Int, Int)],minimum : Map[Long,Int], maximum : Map[Long,Int], id : Long, chr : String, strand : Char, bin : Int, binSize : Long) : List[Grecord] = {
     var start = 0L
     var count = 0
     var countMax = 0
     var recording = false
-
-    basesList.flatMap { current=>
+    si.flatMap { current=>
 
       val newCount = count + current._2
 
-      val newRecording = (newCount >= minimum.get(id).get && newCount <= maximum.get(id).get && basesList.hasNext)
+      val newRecording = (newCount >= minimum.get(id).get && newCount <= maximum.get(id).get && si.hasNext)
 
       val newCountMax =
         if (!newRecording && recording) {
@@ -192,18 +187,15 @@ object GenometricCover {
       val out = if (!newRecording && recording) {
         //output a region
         val end = binSize
-        Some(((id, chr, newStart + bin * binSize,
-          if (current._1 > end) end + bin * binSize else current._1 + bin * binSize,
-          strand, new Array[GValue](0) :+ GDouble(countMax))))
-
+        Some(((id, chr, newStart + bin * binSize, if (current._1 > end) end + bin * binSize else current._1 + bin * binSize, strand, new Array[GValue](0) :+ GDouble(countMax))))
       }else
         None
 
-        count = newCount
-        start = newStart
-        countMax = newCountMax
-        recording = newRecording
-      out
+      count = newCount
+      start = newStart
+      countMax = newCountMax
+      recording = newRecording
+      out //::: coverHelper(newStart, newCount, newCountMax, newRecording, minimum, maximum, i, id, chr, strand, bin, binSize)
     }.toList
   }
 
@@ -214,13 +206,12 @@ object GenometricCover {
     * @param count
     * @param minimum
     * @param maximum
-    * @param basesList
+    * @param i
     */
-  final def histogramHelper(basesList : Iterator[(Int, Int)], minimum : Map[Long,Int], maximum : Map[Long,Int],
-                            id : Long, chr : String, strand : Char, bin : Int, binSize : Long)  : List[Grecord] = {
+  final def histogramHelper(i : Iterator[(Int, Int)], minimum : Map[Long,Int], maximum : Map[Long,Int], id : Long, chr : String, strand : Char, bin : Int, binSize : Long)  : List[Grecord] = {
     var count = 0
     var start = 0L
-    basesList.flatMap { current =>
+    i.flatMap { current =>
       val newCount = count + current._2
       //condition just to check the start of the bin
       if (start.equals(current._1)) {
@@ -250,25 +241,21 @@ object GenometricCover {
 
   /**
     *
-    * @param basesList
+    * @param start
+    * @param count
     * @param minimum
     * @param maximum
-    * @param id
-    * @param chr
-    * @param strand
-    * @param bin
-    * @param binSize
-    * @return
+    * @param growing
+    * @param i
     */
-  final def summitHelper(basesList : Iterator[(Int, Int)], minimum : Map[Long,Int], maximum : Map[Long,Int],
-                         id : Long, chr : String, strand : Char, bin : Int, binSize : Long) :List[Grecord] = {
+  final def summitHelper( i : Iterator[(Int, Int)],  minimum : Map[Long,Int], maximum : Map[Long,Int], id : Long, chr : String, strand : Char, bin : Int, binSize : Long) :List[Grecord] = {
     var start =0L
     var count = 0
     var valid = false
     var reEnteredInValidZoneDecreasing = false
     var growing = false
 
-    basesList.flatMap { current =>
+    i.flatMap { current =>
       val newCount: Int = count + current._2
       val newValid = newCount >= minimum.get(id).getOrElse(1) && newCount <= maximum.get(id).getOrElse(Int.MaxValue)
       val newGrowing: Boolean = newCount > count || (growing && newCount.equals(count))
@@ -319,40 +306,54 @@ object GenometricCover {
     * @param grouping Group IDs
     * @return RDD with group ids set in each sample.
     */
-  def assignGroups(dataset : RDD[GRECORD], grouping : Broadcast[Map[Long, Long]])
-  : RDD[(Long, String, Long, Long, Char, Long, Array[GValue])] = {
+  def assignGroups(dataset : RDD[GRECORD], grouping : Option[Broadcast[Map[Long, Long]]]): RDD[(Long, String, Long, Long, Char, Long, Array[GValue])] = {
     val strands = dataset.map(x=>x._1._5).distinct().collect()
     val doubleStrand = if(strands.size > 2) true else false
     val positiveStrand = if(strands.size == 2 && strands.contains('+') ) true else false
-    val groups = grouping.value
-    dataset.flatMap { x =>
-      val groupId = groups.get(x._1._1).getOrElse(0L)
-
-      if (x._1._5.equals('*') && doubleStrand) {
-        // data is mixed of Positive and Negative strands so the Star strand should be brocken down into two
-        List((groupId, x._1._2, x._1._3, x._1._4, '-', x._1._1, x._2)
-          , (groupId, x._1._2, x._1._3, x._1._4, '+', x._1._1, x._2))
-      } else if (x._1._5.equals('*') && positiveStrand) {
-        //output is Positive strand because the data contains only Positive strand
-        List((groupId, x._1._2, x._1._3, x._1._4, '+', x._1._1, x._2))
-      } else if (x._1._5.equals('*') && !positiveStrand && doubleStrand) {
-        //output is minus strand because the data contains only minus strand
-        List((groupId, x._1._2, x._1._3, x._1._4, '-', x._1._1, x._2))
-      } else // out put will stay as *, plus, minus strand as they are originally
-      {
-        List((groupId, x._1._2, x._1._3, x._1._4, x._1._5, x._1._1, x._2))
+    if(grouping.isDefined)
+    {
+      val groups = grouping.get.value
+      dataset.flatMap { x =>
+        val groupId = groups.get(x._1._1)
+        if (groupId.isDefined)
+          if (x._1._5.equals('*') && doubleStrand) {
+            // data is mixed of Positive and Negative strands so the Star strand should be brocken down into two
+            List((groupId.get, x._1._2, x._1._3, x._1._4, '-', x._1._1, x._2)
+              , (groupId.get, x._1._2, x._1._3, x._1._4, '+', x._1._1, x._2))
+          } else if (x._1._5.equals('*') && positiveStrand) {
+            //output is Positive strand because the data contains only Positive strand
+            List((groupId.get, x._1._2, x._1._3, x._1._4, '+', x._1._1, x._2))
+          } else if (x._1._5.equals('*') && !positiveStrand && doubleStrand) {
+            //output is minus strand because the data contains only minus strand
+            List((groupId.get, x._1._2, x._1._3, x._1._4, '-', x._1._1, x._2))
+          } else // out put will stay as *, plus, minus strand as they are originally
+          {
+            List((groupId.get, x._1._2, x._1._3, x._1._4, x._1._5, x._1._1, x._2))
+          }
+        else {
+          logger.debug("Sample Id: " + x._1._1 + " , Does not belong to any group (specified by GroupBy Condition). ");
+          None
+        }
+      }
+    } else {
+      dataset.flatMap{x =>
+        if (x._1._5.equals('*')&& doubleStrand) {
+          List((0L, x._1._2, x._1._3, x._1._4, '-', x._1._1, x._2)
+            ,(0L, x._1._2, x._1._3, x._1._4, '+', x._1._1, x._2))
+        }else if (x._1._5.equals('*') && positiveStrand) {
+          //output is Positive strand because the data contains only Positive strand
+          List((0L, x._1._2, x._1._3, x._1._4, '+', x._1._1, x._2))
+        } else if (x._1._5.equals('*') && !positiveStrand && doubleStrand) {
+          //output is minus strand because the data contains only minus strand
+          List((0L, x._1._2, x._1._3, x._1._4, '-', x._1._1, x._2))
+        } else {
+          List((0L, x._1._2, x._1._3, x._1._4, x._1._5, x._1._1, x._2))
+        }
       }
     }
   }
 
-  /**
-    *
-    * @param dataset RDD of the input dataset
-    * @param binSize describes the size of the bin as a long value
-    * @return
-    */
-  def extractor(dataset : RDD[(Long, String, Long, Long, Char, Long, Array[GValue])], binSize : Long)
-  : RDD[((String, Int, Long, Char), HashMap[Int, Int])] = {
+  def extractor(dataset : RDD[(Long, String, Long, Long, Char, Long, Array[GValue])], binSize : Long) = {
     dataset.flatMap{x =>
       val startBin =(x._3/binSize).toInt
       val stopBin = (x._4/binSize).toInt
@@ -361,11 +362,15 @@ object GenometricCover {
         List(((x._2, startBin, x._1,x._5), HashMap((x._3 - startBin * binSize).toInt -> 1, (x._4 - startBin * binSize).toInt -> -1)))
       } else{
         val map_start  = ((x._2,startBin,x._1,x._5),HashMap((x._3 - startBin * binSize).toInt -> 1, binSize.toInt -> -1))
-        val map_stop   = ((x._2,stopBin, x._1,x._5),HashMap(0 -> 1, (x._4 - stopBin  * binSize).toInt -> -1))
+        val map_stop   = ((x._2,stopBin, x._1,x._5),HashMap((x._4 - stopBin  * binSize).toInt -> -1, 0 -> 1))
         val map_int = for (i <- startBin+1 to stopBin-1) yield (( x._2, i,x._1, x._5), HashMap(0->1,binSize.toInt-> -1))
 
         List(map_start, map_stop) ++ map_int
       }
     }
   }
+
+
+
+
 }
