857d911d504bfd9041ac79f207f26227d149f6e2
lucananni93 <luca.nanni10@studio.unibo.it>
2017-06-14 16:58:29 +0200

    Serialization and Deserialization utilities

--- GMQL-Core/src/main/scala/it/polimi/genomics/core/Utilities.scala
+++ GMQL-Core/src/main/scala/it/polimi/genomics/core/Utilities.scala
@@ -1,16 +1,15 @@
-package it.polimi.genomics.pythonapi.remote
+package it.polimi.genomics.core
 
-import java.io.{ByteArrayInputStream, ObjectInputStream, ObjectOutputStream}
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
 import java.util.Base64
 
 import it.polimi.genomics.core.DataStructures.IRVariable
-import org.apache.commons.io.output.ByteArrayOutputStream
 
 /**
-  * Created by Luca Nanni on 01/06/17.
+  * Created by Luca Nanni on 06/06/17.
   * Email: luca.nanni@mail.polimi.it
   */
-object DAGSerializer {
+object Utilities {
 
   def serializeToBase64(variableList : List[IRVariable]): String = {
     val baos = new ByteArrayOutputStream()
@@ -20,7 +19,7 @@ object DAGSerializer {
     Base64.getEncoder.encodeToString(baos.toByteArray)
   }
 
-  def deserializeToMaterializationList(serialized: String): List[IRVariable] = {
+  def deserializeDAG(serialized: String): List[IRVariable] = {
     val data = Base64.getDecoder.decode(serialized)
     val ois = new ObjectInputStream(new ByteArrayInputStream(data))
     val deserialized = ois.readObject().asInstanceOf[List[IRVariable]]
