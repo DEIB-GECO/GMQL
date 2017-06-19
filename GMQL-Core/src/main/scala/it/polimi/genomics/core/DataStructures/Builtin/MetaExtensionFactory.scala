33b22fdd3b6ec7e67c3122e1bd766e53c0890477
lucananni93 <luca.nanni10@studio.unibo.it>
2017-06-19 16:18:45 +0200

    Changed return type to MetaExtension

--- GMQL-Core/src/main/scala/it/polimi/genomics/core/DataStructures/Builtin/MetaExtensionFactory.scala
+++ GMQL-Core/src/main/scala/it/polimi/genomics/core/DataStructures/Builtin/MetaExtensionFactory.scala
@@ -1,10 +1,10 @@
 package it.polimi.genomics.core.DataStructures.Builtin
 
-import it.polimi.genomics.core.DataStructures.MetaAggregate.{MENode, MetaAggregateStruct}
+import it.polimi.genomics.core.DataStructures.MetaAggregate.{MENode, MetaExtension}
 
 /**
   * Created by Olga Gorlova on 07.06.2017.
   */
 trait MetaExtensionFactory {
-  def get(dag : MENode, output : String) : MetaAggregateStruct
+  def get(dag : MENode, output : String) : MetaExtension
 }
