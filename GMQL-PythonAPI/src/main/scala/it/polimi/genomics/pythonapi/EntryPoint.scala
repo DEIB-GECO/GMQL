f5cd74acd2452bfb914f609a14249775505a47d6
lucananni93 <luca.nanni10@studio.unibo.it>
2017-07-21 14:58:37 +0200

    added driver host

--- GMQL-PythonAPI/src/main/scala/it/polimi/genomics/pythonapi/EntryPoint.scala
+++ GMQL-PythonAPI/src/main/scala/it/polimi/genomics/pythonapi/EntryPoint.scala
@@ -59,6 +59,7 @@ object EntryPoint {
         .set("spark.driver.memory", properties.driverMemory)
         .set("spark.kryoserializer.buffer.max", properties.kryobuffer)
         .set("spark.driver.maxResultSize", properties.maxResultSize)
+        .set("spark.driver.host", properties.driverHost)
 
     val sc = SparkContext.getOrCreate(conf)
     logger.info("Spark Context initiated")
