393a41bb254305b4d09ee579debf1e8d6fca9185
lucananni93 <luca.nanni10@studio.unibo.it>
2017-09-08 01:39:21 +0200

    Changed val to var for dag renaming

--- GMQL-Core/src/main/scala/it/polimi/genomics/core/DataStructures/IRDataSet.scala
+++ GMQL-Core/src/main/scala/it/polimi/genomics/core/DataStructures/IRDataSet.scala
@@ -2,5 +2,5 @@ package it.polimi.genomics.core.DataStructures
 
 import it.polimi.genomics.core.ParsingType.PARSING_TYPE
 
-case class IRDataSet(position:String,
+case class IRDataSet(var position:String,
                      schema:java.util.List[(String,PARSING_TYPE)])
