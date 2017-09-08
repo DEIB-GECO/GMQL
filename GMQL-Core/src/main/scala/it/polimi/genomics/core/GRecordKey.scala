834e858367887b30f66b97ed65c3e1e5b7f351ad
akaitoua <abdulrahman.kaitoua@polimi.it>
2017-09-08 10:53:39 +0200

    solving [Issue #63].

--- GMQL-Core/src/main/scala/it/polimi/genomics/core/GRecordKey.scala
+++ GMQL-Core/src/main/scala/it/polimi/genomics/core/GRecordKey.scala
@@ -5,7 +5,7 @@ package it.polimi.genomics.core
  * Email: abdulrahman.kaitoua@polimi.it
  *
  */
-class GRecordKey (id:Long, chrom:String,start:Long,stop:Long,strand:Char)
+class GRecordKey (val id:Long, val chrom:String,val start:Long,val stop:Long,val strand:Char)
   extends Tuple5(id,chrom,start,stop,strand)
   with Ordered[GRecordKey]
   with Serializable
@@ -13,15 +13,15 @@ class GRecordKey (id:Long, chrom:String,start:Long,stop:Long,strand:Char)
   def this()= this(0,"chr",0,0,'.')
 
   def compare(o: GRecordKey): Int = {
-    if (this.id.equals(o._1))
-      if (this.chrom.equals(o._2))
-        if (this.start == o._3)
-          if (this.stop == o._4)
-            this.stop compare o._5
-          else this.stop compare o._4
-        else this.start compare o._3
-      else this.chrom compare o._2
-    else this.id compare o._1
+      if (this.id == o.id)
+       if (this.chrom.equals(o.chrom))
+        if (this.start == o.start)
+          if (this.stop == o.stop)
+            this.strand compare o.strand
+          else this.stop compare o.stop
+        else this.start compare o.start
+      else this.chrom compare o.chrom
+    else this.id compare o.id
   }
 
   override def toString ():String ={
