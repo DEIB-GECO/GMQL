7d5a4a8e9640a44ca2054d9368a6e8fe5a1d0c82
pp86 <pietro.pinoli@gmail.com>
2017-12-06 12:59:18 +0100

    Makes possible to update value of CHR and STRAND This fixes issue #84

--- Compiler/src/main/scala/it/polimi/genomics/compiler/GmqlParsers.scala
+++ Compiler/src/main/scala/it/polimi/genomics/compiler/GmqlParsers.scala
@@ -342,11 +342,13 @@ trait GmqlParsers extends JavaTokenParsers {
       } |
     (
       (
-        not(RIGHT | LEFT | START | STOP) ~> any_field_identifier |
+        not(RIGHT | LEFT | START | STOP | CHR | STRAND) ~> any_field_identifier |
         RIGHT ^^ {x => FieldPosition(COORD_POS.RIGHT_POS)} |
         LEFT ^^ {x => FieldPosition(COORD_POS.LEFT_POS)} |
         START ^^ {x => FieldPosition(COORD_POS.START_POS)} |
-        STOP ^^ {x => FieldPosition(COORD_POS.STOP_POS)}
+        STOP ^^ {x => FieldPosition(COORD_POS.STOP_POS)} |
+        STRAND ^^ {x => FieldPosition(COORD_POS.STRAND_POS)} |
+        CHR ^^ {x => FieldPosition(COORD_POS.CHR_POS)}
         ) <~ AS
       ) ~ re_expr ^^ { x => RegionModifier(x._1,x._2)}
 
