c4b77e46eec26520b684d92808d6c005e54cf44c
acanakoglu <acanak@gmail.com>
2018-02-11 16:39:30 +0100

    optimized the code

--- GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/RegionsOperators/GenometricMap/GenometricMap71.scala
+++ GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/RegionsOperators/GenometricMap/GenometricMap71.scala
@@ -11,6 +11,7 @@ import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
 import org.slf4j.LoggerFactory
 
+import scala.collection.mutable.ArrayBuffer
 import scala.util.hashing.MurmurHash3
 
 
@@ -41,7 +42,7 @@ object GenometricMap71 {
     execute(executor, grouping, aggregator, ref, exp, binningParameter, REF_PARALLILISM, sc)
   }
 
-  case class MapKey(sampleId: Long, refId: Long, refChr: String, refStart: Long, refStop: Long, refStrand: Char, refValues: List[GValue]) {
+  case class MapKey(/*sampleId: Long, */newId: Long, refChr: String, refStart: Long, refStop: Long, refStrand: Char, refValues: List[GValue]) {
 
     override def equals(obj: scala.Any): Boolean = {
       if (## == obj.##) {
@@ -50,8 +51,8 @@ object GenometricMap71 {
         this.refStart == that.refStart &&
           this.refStop == that.refStop &&
           this.refStrand == that.refStrand &&
-          this.sampleId == that.sampleId &&
-          this.refId == that.refId &&
+//          this.sampleId == that.sampleId &&
+          this.newId == that.newId &&
           this.refChr == that.refChr &&
           this.refValues == that.refValues
       }
@@ -61,6 +62,8 @@ object GenometricMap71 {
 
     @transient override lazy val hashCode: Int = MurmurHash3.productHash(this)
   }
+
+  //possible solution is generate GRecordKey and add the others as list
 
   @throws[SelectFormatException]
   def execute(executor: GMQLSparkExecutor, grouping: OptionalMetaJoinOperator, aggregator: List[RegionAggregate.RegionsToRegion], ref: RDD[GRECORD], exp: RDD[GRECORD], BINNING_PARAMETER: Long, REF_PARALLILISM: Int, sc: SparkContext): RDD[GRECORD] = {
@@ -74,60 +77,73 @@ object GenometricMap71 {
     val expBinned = exp.binDS(BINNING_PARAMETER, aggregator)
     val refBinnedRep = ref.repartition(160).binDS(BINNING_PARAMETER, refGroups)
 
+    val emptyArrayGValue = Array.empty[GValue]
+    val emptyArrayInt = Array.empty[Int]
 
-    val RefExpJoined =
+
+    val indexedAggregator = aggregator.zipWithIndex
+
+    val reduceFunc: ((Array[GValue], Int, Array[Int]), (Array[GValue], Int, Array[Int])) => (Array[GValue], Int, Array[Int]) = {
+      case ((leftValues: Array[GValue], leftCount: Int, leftCounts: Array[Int]), (rightValues: Array[GValue], rightCount: Int, rightCounts: Array[Int])) =>
+        val values: Array[GValue] =
+          if (leftValues.nonEmpty && rightValues.nonEmpty) {
+            indexedAggregator.map { case (a, i) =>
+              a.fun(List(leftValues(i), rightValues(i)))
+            }.toArray
+          } else if (rightValues.nonEmpty)
+            rightValues
+          else
+            leftValues
+
+        (values, leftCount + rightCount, leftCounts.zipAll(rightCounts, 0, 0).map(s => s._1 + s._2))
+    }
+
+    val RefExpJoined: RDD[(MapKey, (Array[GValue], Int, Array[Int]))] =
       refBinnedRep
         .cogroup(expBinned)
         .flatMap {
           case (key: (Long, String, Int), (ref: Iterable[(Long, Long, Long, Char, Array[GValue])], exp: Iterable[(Long, Long, Char, Array[GValue])])) =>
             // key: (Long, String, Int) sampleId, chr, bin
-            // ref: Iterable[(10, Long, Long, Char, Array[GValue])] sampleId, start, stop, strand, others
+            // ref: Iterable[(Long, Long, Long, Char, Array[GValue])] newSampleId, start, stop, strand, others
             // exp: Iterable[(Long, Long, Char, Array[GValue])] start, stop, strand, others
-            ref.flatMap { refRecord =>
-              lazy val mapKey = MapKey(key._1, refRecord._1, key._2, refRecord._2, refRecord._3, refRecord._4, refRecord._5.toList)
 
-              val refInStartBin = (refRecord._2 / BINNING_PARAMETER).toInt.equals(key._3)
-              val isRefStrandBoth = refRecord._4.equals('*')
-              val expTemp = exp.flatMap { expRecord =>
-                if (
-                /* space overlapping */
-                  (refRecord._2 < expRecord._2 && expRecord._1 < refRecord._3)
-                    && /* same strand */
-                    (isRefStrandBoth || expRecord._3.equals('*') || refRecord._4.equals(expRecord._3))
-                    && /* first comparison (start bin of either the ref or exp)*/
-                    (refInStartBin || (expRecord._1 / BINNING_PARAMETER).toInt.equals(key._3))
-                )
-                //if there is a overlapping on the same strand and one of the regions(ref or exp) starts at this bin
-                  Some((mapKey, (expRecord._4, 1, expRecord._4.map(s => if (s.isInstanceOf[GNull]) 0 else 1))))
+            ref
+              .iterator
+              .map { refRecord =>
+                val mapKey = MapKey(/*key._1,*/ refRecord._1, key._2, refRecord._2, refRecord._3, refRecord._4, refRecord._5.toList)
+
+                val refInStartBin = (refRecord._2 / BINNING_PARAMETER).toInt.equals(key._3)
+                val isRefStrandBoth = refRecord._4.equals('*')
+
+                val expFiltered = exp
+                  .iterator
+                  .filter(expRecord =>
+                    (/*space overlapping*/
+                      refRecord._2 < expRecord._2 && expRecord._1 < refRecord._3) &&
+                      /* same strand */
+                      (isRefStrandBoth || expRecord._3.equals('*') || refRecord._4.equals(expRecord._3)) &&
+                      /* first comparison (start bin of either the ref or exp)*/
+                      (refInStartBin || (expRecord._1 / BINNING_PARAMETER).toInt.equals(key._3))
+                  )
+
+                if (expFiltered.nonEmpty) { //if there is a match ref against exp
+                  val expReduced: (Array[GValue], Int, Array[Int]) = expFiltered
+                    .map(expRecord => (expRecord._4, 1, expRecord._4.map(s => if (s.isInstanceOf[GNull]) 0 else 1)))
+                    .reduce(reduceFunc)
+                  (mapKey, expReduced)
+                }
+                else if (refInStartBin) //if there is not match and in order to add one time ref we check if ref starts here
+                  (mapKey, (emptyArrayGValue, 0, emptyArrayInt))
                 else
-                  None
-              }
+                  (mapKey, (emptyArrayGValue, -1, emptyArrayInt))
 
-              if (expTemp.nonEmpty) //if there is a match ref against exp
-                expTemp
-              else if (refInStartBin) //if there is not match and in order to add one time ref we check if ref starts here
-                Some((mapKey, (Array.empty[GValue], 0, Array.empty[Int])))
-              else
-                None
-            }
+              }.filter(_._2._2 != -1)
         }
 
-    val reduced = RefExpJoined.reduceByKey { case ((leftValues, leftCount, leftCounts), (rightValues, rightCount, rightCounts)) =>
-      val values: Array[GValue] =
-        if (leftValues.nonEmpty && rightValues.nonEmpty) {
-          aggregator.zipWithIndex.map { case (a, i) =>
-            a.fun(List(leftValues(i), rightValues(i)))
-          }.toArray
-        } else if (rightValues.nonEmpty)
-          rightValues
-        else
-          leftValues
+    val reduced = RefExpJoined.reduceByKey(reduceFunc)
 
-      (values, leftCount + rightCount, leftCounts.zip(rightCounts).map(s => s._1 + s._2))
-    }
-
-    val output = reduced.map { case (mapKey, (values, count, counts)) =>
-      val newVal = aggregator.zipWithIndex.map { case (f, i) =>
+    val output: RDD[(GRecordKey, Array[GValue])] = reduced.map { case (mapKey, (values, count, counts)) =>
+      val newVal = indexedAggregator.map { case (f, i) =>
         val value: GValue =
           if (values.nonEmpty)
             values(i)
@@ -136,11 +152,18 @@ object GenometricMap71 {
         f.funOut(value, (count, if (counts.nonEmpty) counts(i) else 0))
       }
 
-      val newID = Hashing.md5().newHasher().putLong(mapKey.refId).putLong(mapKey.sampleId).hash().asLong
+      val newID = mapKey.newId //Hashing.md5().newHasher().putLong(mapKey.refId).putLong(mapKey.sampleId).hash().asLong
 
       val gRecordKey = GRecordKey(newID, mapKey.refChr, mapKey.refStart, mapKey.refStop, mapKey.refStrand)
 
-      (gRecordKey, ((mapKey.refValues :+ GDouble(count)) ++ newVal).toArray)
+      //default size is 16
+      val buffer = new ArrayBuffer[GValue]
+
+      buffer ++= mapKey.refValues
+      buffer += GDouble(count)
+      buffer ++= newVal
+
+      (gRecordKey, buffer.toArray)
     }
 
     output
@@ -150,47 +173,31 @@ object GenometricMap71 {
     def binDS(bin: Long, aggregator: List[RegionAggregate.RegionsToRegion]): RDD[((Long, String, Int), (Long, Long, Char, Array[GValue]))] =
       rdd.flatMap { x =>
         //        if (bin > 0) {
-        val startbin = (x._1._3 / bin).toInt
-        val stopbin = (x._1._4 / bin).toInt
+        val startBin = (x._1._3 / bin).toInt
+        val stopBin = (x._1._4 / bin).toInt
+
         val newVal: Array[GValue] = aggregator
           .map((f: RegionAggregate.RegionsToRegion) => {
             x._2(f.index)
           }).toArray
-        //          println (newVal.mkString("/"))
-        for (i <- startbin to stopbin)
-          yield ((x._1._1, x._1._2, i), (x._1._3, x._1._4, x._1._5, newVal))
-        //        } else
-        //          {
-        //            val newVal: Array[GValue] = aggregator
-        //              .map((f: RegionAggregate.RegionsToRegion) => {
-        //                x._2(f.index)
-        //              }).toArray
-        //            //          println (newVal.mkString("/"))
-        //              Some((x._1._1, x._1._2, 0), (x._1._3, x._1._4, x._1._5, newVal))
-        //          }
+
+        (startBin to stopBin).map(i => ((x._1._1, x._1._2, i), (x._1._3, x._1._4, x._1._5, newVal)))
       }
 
     def binDS(bin: Long, Bgroups: Broadcast[collection.Map[Long, Iterable[Long]]]): RDD[((Long, String, Int), (Long, Long, Long, Char, Array[GValue]))] =
-    //        rdd.keyBy(x=>x._1._1).join(Bgroups).flatMap { x =>
-    //        if (bin > 0) {
-    //            val startbin = (x._2._1._1._3 / bin).toInt
-    //            val stopbin = (x._2._1._1._4 / bin).toInt
-    //              (startbin to stopbin).map(i =>
-    //                ((x._2._2, x._2._1._1._2, i), (x._2._1._1._1, x._2._1._1._3, x._2._1._1._4, x._2._1._1._5, x._2._1._2))
-    //              )
-    //        }else
-    //             Some((x._2._2, x._2._1._1._2, 0), (x._2._1._1._1, x._2._1._1._3, x._2._1._1._4, x._2._1._1._5, x._2._1._2))
-    //      }
       rdd.flatMap { x =>
-        val startbin = (x._1._3 / bin).toInt
-        val stopbin = (x._1._4 / bin).toInt
+        val startBin = (x._1._3 / bin).toInt
+        val stopBin = (x._1._4 / bin).toInt
 
-        (startbin to stopbin).flatMap { i =>
-          Bgroups.value.getOrElse(x._1._1, Iterable[Long]())
-            .map(exp_id =>
-              ((exp_id, x._1._2, i), (x._1._1, x._1._3, x._1._4, x._1._5, x._2))
-            )
+
+        Bgroups.value.getOrElse(x._1._1, Iterable[Long]()).flatMap { exp_id =>
+          val newID = Hashing.md5().newHasher().putLong(x._1._1).putLong(exp_id).hash().asLong
+
+          (startBin to stopBin).map { i =>
+            ((exp_id, x._1._2, i), (newID, x._1._3, x._1._4, x._1._5, x._2))
+          }
         }
+
       }
 
 
