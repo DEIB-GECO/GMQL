33ba20c96833dfc57d5f8f64c205b85327bec7cf
acanakoglu <acanak@gmail.com>
2018-03-05 15:35:52 +0100

    map71 sort has been changed

--- GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/RegionsOperators/GenometricMap/GenometricMap71.scala
+++ GMQL-Spark/src/main/scala/it/polimi/genomics/spark/implementation/RegionsOperators/GenometricMap/GenometricMap71.scala
@@ -11,6 +11,7 @@ import org.apache.spark.rdd.RDD
 import org.slf4j.LoggerFactory
 
 import scala.collection.mutable.ArrayBuffer
+import scala.math.Ordering
 import scala.util.hashing.MurmurHash3
 
 
@@ -70,14 +71,13 @@ object GenometricMap71 {
 
     val refGroups = sc.broadcast(groups.groupByKey().collectAsMap())
 
-    val refNewIds = refGroups.value.map { case ( refId:Long, expIds: Iterable[Long]) =>
-      val newIds = expIds.map{expId =>
-        val newId= Hashing.md5().newHasher().putLong(refId).putLong(expId).hash().asLong
+    val refNewIds = refGroups.value.map { case (refId: Long, expIds: Iterable[Long]) =>
+      val newIds = expIds.map { expId =>
+        val newId = Hashing.md5().newHasher().putLong(refId).putLong(expId).hash().asLong
         (expId, newId)
       }
       (refId, newIds)
     }.toMap
-
 
 
     implicit val orderGRECORD: Ordering[(GRecordKey, Array[GValue])] = Ordering.by { ar: GRECORD => ar._1 }
@@ -115,9 +115,34 @@ object GenometricMap71 {
             // ref: Iterable[(Long, Long, Long, Char, Array[GValue])] newSampleId, start, stop, strand, others
             // exp: Iterable[(Long, Long, Char, Array[GValue])] start, stop, strand, others
 
-            val refSorted = ref.toList.sortBy(_._2)
-            val expSorted = exp.toList.sortBy(_._1).toVector
-            var firstIndex : Int = 0
+
+            val refSorted = {
+              val ord = new Ordering[(Long, Long, Long, Char, Array[GValue])] {
+                override def compare(x: (Long, Long, Long, Char, Array[GValue]), y: (Long, Long, Long, Char, Array[GValue])): Int =
+                  x._2 compare y._2
+              }
+
+              val array = ref.toArray
+              java.util.Arrays.sort(array, ord.asInstanceOf[Ordering[Object]])
+              array
+            }
+
+            val expSorted = {
+              val ord = new Ordering[(Long, Long, Long, Char, Array[GValue])] {
+                override def compare(x: (Long, Long, Long, Char, Array[GValue]), y: (Long, Long, Long, Char, Array[GValue])): Int =
+                  x._1 compare y._1
+              }
+
+              val array = exp.toArray
+              java.util.Arrays.sort(array, ord.asInstanceOf[Ordering[Object]])
+              array
+            }
+
+
+
+//            val refSorted2 = ref.toList.sortBy(_._2)
+//            val expSorted2 = exp.toList.sortBy(_._1).toVector
+            var firstIndex: Int = 0
 
 
             refSorted
@@ -129,24 +154,24 @@ object GenometricMap71 {
                 val isRefStrandBoth = refRecord._4.equals('*')
 
 
-                while (firstIndex < expSorted.size  && expSorted(firstIndex)._2 <= refRecord._2) {
+                while (firstIndex < expSorted.size && expSorted(firstIndex)._2 <= refRecord._2) {
                   firstIndex += 1
                 }
 
                 var index = firstIndex
-                var expFiltered : List[(Long, Long, Char, Array[GValue])] = Nil
+                var expFiltered: List[(Long, Long, Char, Array[GValue])] = Nil
 
-                while (index < expSorted.size && expSorted(index)._1 < refRecord._3){
-                  if (/*space overlapping*/
+                while (index < expSorted.size && expSorted(index)._1 < refRecord._3) {
+                  if ( /*space overlapping*/
                     refRecord._2 < expSorted(index)._2 &&
                       /* same strand */
                       (isRefStrandBoth || expSorted(index)._3.equals('*') || refRecord._4.equals(expSorted(index)._3)) &&
                       /* first comparison (start bin of either the ref or exp)*/
-                      (refInStartBin || (expSorted(index)._1 / BINNING_PARAMETER).toInt.equals(key._3))){
+                      (refInStartBin || (expSorted(index)._1 / BINNING_PARAMETER).toInt.equals(key._3))) {
                     expFiltered = expSorted(index) :: expFiltered
 
                   }
-                  index += index +1
+                  index += index + 1
                 }
 
 
@@ -212,7 +237,7 @@ object GenometricMap71 {
         val startBin = (x._1._3 / bin).toInt
         val stopBin = (x._1._4 / bin).toInt
 
-        refNewIds.getOrElse(x._1._1, Iterable.empty).flatMap { case (expId,newId) =>
+        refNewIds.getOrElse(x._1._1, Iterable.empty).flatMap { case (expId, newId) =>
           (startBin to stopBin).map { i =>
             ((expId, x._1._2, i), (newId, x._1._3, x._1._4, x._1._5, x._2))
           }
