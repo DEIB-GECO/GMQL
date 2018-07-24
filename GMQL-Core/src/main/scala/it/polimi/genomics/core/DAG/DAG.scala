82263cd36ffb06f9340f8aa96b4c357007a7d8ce
lucananni93 <lucananni93@gmail.com>
2018-07-24 15:49:45 +0200

    code restructuring

--- GMQL-Core/src/main/scala/it/polimi/genomics/core/DAG/DAG.scala
+++ GMQL-Core/src/main/scala/it/polimi/genomics/core/DAG/DAG.scala
@@ -3,39 +3,10 @@ package it.polimi.genomics.core.DAG
 import it.polimi.genomics.core.DataStructures.{IROperator, IRVariable}
 
 class DAG (val raw: List[IROperator]) {
-
   private val depthWidth: collection.mutable.Map[Int, Int] = collection.mutable.Map[Int, Int]()
 
   // Auxiliary constructor
   def this(raw: => List[IRVariable]) =  this( raw.flatMap(x=>  List[IROperator](x.regionDag, x.metaDag)) )
-
-
-  // notice that this method received a tree : non-empty single-root DAG
-
-  /**
-    * Recursive private function
-    * @param nodeClass Class of IRVariable to be matched
-    * @param tree a non-empty DAG with a single root node
-    * @return DAG containing all the sub-trees of 'tree' s.t. their root is an instance
-    *         of class 'nodeClass'; the result may contain 'tree' itself
-    */
-  private def _subDAG( nodeClass: Class[_], tree: DAG) : DAG  = {
-
-    val root = tree.raw.head
-
-    var result: DAG =
-    if( root.getClass equals nodeClass ) {
-      new DAG( List(root) )
-    } else {
-      new DAG( List[IROperator]() )
-    }
-
-    for ( child <- root.getDependencies ) {
-      result = result.union( _subDAG(nodeClass, new DAG( List(child) )) )
-    }
-
-    result
-  }
 
   // computes the depthWith Map, for each depth the number of nodes at that depth
   private def _depthWidth(root: IROperator, depth: Int): Unit = {
@@ -44,36 +15,22 @@ class DAG (val raw: List[IROperator]) {
      _depthWidth(child, depth+1)
     }
 
-    depthWidth(depth) =
-    if( depthWidth.contains(depth) ) {
-      depthWidth(depth)+1
-    } else {
-      1
-    }
+    depthWidth(depth) = if( depthWidth.contains(depth) ) depthWidth(depth)+1 else 1
   }
 
-  private def _markDown(node:IROperator): Unit = {
-    node.requiresOutputProfile = true
-    node.getDependencies.foreach(_markDown)
-  }
-
-
-  /**
-    * Returns a DAG in which all nodes in the original DAG of class
-    * nodeClass become root nodes of the new DAG
-    * @param nodeClass
-    * @return
+  /** Returns a DAG in which all the nodes of the original DAG which
+    * satisfy the given predicate become root nodes of the new DAG
+    *
+    * @param pred: a predicate on the IROperator
+    * @return a new DAG
     */
-  def subDAG( nodeClass : Class[_] ): DAG = {
-    raw.map(x => _subDAG(nodeClass,  new DAG( List(x) ) ) ).reduce( (x,y) => x.union(y) )
+  def subDAG(pred: IROperator => Boolean): DAG = {
+    def _subDAG(ops: List[IROperator], pred:IROperator => Boolean): List[IROperator] = {
+      if (ops.isEmpty) List[IROperator]() else ops.filter(pred) ++ _subDAG(ops.flatMap(x => x.getDependencies), pred)
+    }
+    _subDAG(this.raw, pred).map(x => new DAG(List(x))).reduce( (x,y) => x.union(y) )
   }
 
-//  def subDAG(pred: IROperator => Boolean): DAG = {
-//    def _subdDAG(dag: DAG, pred:IROperator => Boolean): DAG = {
-//
-//    }
-//    raw.map(x => )
-//  }
 
   def union( other : DAG ) : DAG  = {
     new DAG( raw.union( other.raw ).distinct ) //check it is expectd result
@@ -81,13 +38,16 @@ class DAG (val raw: List[IROperator]) {
 
   /**
     * Set requirement for profile estimation on the descendants of nodeClass
-    * @param nodeClass
-    * @return
+    * @param pred a predicate on the IROperator
+    * @return nothing
     */
-  def markDown( nodeClass : Class[_] ): Unit = { subDAG(nodeClass).raw.map(_markDown) }
-
-
-  def getLeaves(): DAG = ???
+  def markDown( pred: IROperator => Boolean ): Unit = {
+    def _markDown(node:IROperator): Unit = {
+      node.requiresOutputProfile = true
+      node.getDependencies.foreach(_markDown)
+    }
+    subDAG(pred).raw.map(_markDown)
+  }
 
   /**
     *  mMximum number of elements having the same depth
@@ -121,5 +81,37 @@ class DAG (val raw: List[IROperator]) {
     new DAGView(this, title)
 
   }
+
+  //  /**
+  //    * Returns a DAG in which all nodes in the original DAG of class
+  //    * nodeClass become root nodes of the new DAG
+  //    * @param nodeClass
+  //    * @return
+  //    */
+  //  def subDAG( nodeClass : Class[_] ): DAG = {
+  //
+  //    /**
+  //      * Recursive private function
+  //      * @param nodeClass Class of IRVariable to be matched
+  //      * @param tree a non-empty DAG with a single root node
+  //      * @return DAG containing all the sub-trees of 'tree' s.t. their root is an instance
+  //      *         of class 'nodeClass'; the result may contain 'tree' itself
+  //      */
+  //    def _subDAG( nodeClass: Class[_], tree: DAG) : DAG  = {
+  //      val root = tree.raw.head
+  //      var result: DAG =
+  //        if( root.getClass equals nodeClass ) {
+  //          new DAG( List(root) )
+  //        } else {
+  //          new DAG( List[IROperator]() )
+  //        }
+  //      for ( child <- root.getDependencies ) {
+  //        result = result.union( _subDAG(nodeClass, new DAG( List(child) )) )
+  //      }
+  //      result
+  //    }
+  //
+  //    raw.map(x => _subDAG(nodeClass,  new DAG( List(x) ) ) ).reduce( (x,y) => x.union(y) )
+  //  }
 
 }
