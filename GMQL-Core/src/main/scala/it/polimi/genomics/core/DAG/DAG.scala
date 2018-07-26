a097dabe7a1bb5d9a00569e4427229baaaaf5647
lucananni93 <lucananni93@gmail.com>
2018-07-26 14:44:51 +0200

    added "toIRVariables"

--- GMQL-Core/src/main/scala/it/polimi/genomics/core/DAG/DAG.scala
+++ GMQL-Core/src/main/scala/it/polimi/genomics/core/DAG/DAG.scala
@@ -1,22 +1,57 @@
 package it.polimi.genomics.core.DAG
 
-import it.polimi.genomics.core.DataStructures.{IROperator, IRVariable}
+import it.polimi.genomics.core.DataStructures.ExecutionParameters.BinningParameter
+import it.polimi.genomics.core.DataStructures._
+import it.polimi.genomics.core.ParsingType.PARSING_TYPE
 
 class DAG (val raw: List[IROperator]) {
   private val depthWidth: collection.mutable.Map[Int, Int] = collection.mutable.Map[Int, Int]()
-
   // Auxiliary constructor
   def this(raw: => List[IRVariable]) =  this( raw.flatMap(x=>  List[IROperator](x.regionDag, x.metaDag)) )
 
-  // computes the depthWith Map, for each depth the number of nodes at that depth
-  private def _depthWidth(root: IROperator, depth: Int): Unit = {
 
-    for ( child <- root.getDependencies ) {
-     _depthWidth(child, depth+1)
-    }
-
-    depthWidth(depth) = if( depthWidth.contains(depth) ) depthWidth(depth)+1 else 1
+  // For a DAG to be Variable-consistent it must not have any root node which is different from
+  // IRStoreMD and IRStoreRD
+  private def checkVariableConsistency: Boolean = !this.raw.exists {
+    case IRStoreRD(_, _, _, _, _) | IRStoreMD(_, _, _) => false
+    case _ => true
   }
+
+  /**
+    * Returns the DAG in the form of List of IRVariables
+    * @param binning implicit parameter
+    * @return a list of IRVariable
+    */
+  def toVariables(implicit binning: BinningParameter): List[IRVariable] = {
+    if(checkVariableConsistency) {
+      def getMap(op: IROperator): (IRDataSet, IROperator) = {
+        op match {
+          case IRStoreMD(_, _, dataSet) => (dataSet, op)
+          case IRStoreRD(_, _, _, _, dataSet) => (dataSet, op)
+        }
+      }
+      // Grouping IRVariables together
+      val outputDatasets: Map[IRDataSet, List[IROperator]] = this.raw.map(getMap)
+        .groupBy( _._1 ).mapValues(_.map(x => x._2))
+
+      def getDagParts(l: List[IROperator]): (IRStoreMD, IRStoreRD, List[(String, PARSING_TYPE)]) = {
+        if(l.length != 2)
+          throw new IllegalStateException("A dataset is an output of multiple IRVariables!")
+        else{
+          val metaDag = l.filter { case IRStoreMD(_, _, _) => true; case _ => false }.head.asInstanceOf[IRStoreMD]
+          val regionDag = l.filter { case IRStoreRD(_, _, _, _, _) => true; case _ => false }.head.asInstanceOf[IRStoreRD]
+          (metaDag,regionDag,regionDag.schema)
+        }
+      }
+      outputDatasets.map(x => {
+        val p = getDagParts(x._2)
+        IRVariable(p._1, p._2, p._3)(binning)
+      }).toList
+    }
+    else
+      throw new IllegalStateException("The DAG cannot be casted to List[IRVarialbe]")
+  }
+
 
   /** Returns a DAG in which all the nodes of the original DAG which
     * satisfy the given predicate become root nodes of the new DAG
@@ -49,6 +84,15 @@ class DAG (val raw: List[IROperator]) {
     subDAG(pred).raw.map(_markDown)
   }
 
+  // computes the depthWith Map, for each depth the number of nodes at that depth
+  private def _depthWidth(root: IROperator, depth: Int): Unit = {
+
+    for ( child <- root.getDependencies ) {
+      _depthWidth(child, depth+1)
+    }
+    depthWidth(depth) = if( depthWidth.contains(depth) ) depthWidth(depth)+1 else 1
+  }
+
   /**
     *  mMximum number of elements having the same depth
     * @return
@@ -61,7 +105,7 @@ class DAG (val raw: List[IROperator]) {
   //  val depth = depthWidth.filter( x => x._2==max )
   }
 
-  def widthAt(depth: Int) = {
+  def widthAt(depth: Int): Int = {
     if( depthWidth.isEmpty ) {
       raw.foreach(x => _depthWidth( x , 0 ))
     }
@@ -69,14 +113,14 @@ class DAG (val raw: List[IROperator]) {
 
   }
 
-  def depth() = {
+  def depth(): Int = {
     if( depthWidth.isEmpty ) {
       raw.foreach(x => _depthWidth( x , 0 ))
     }
     depthWidth.keys.max
   }
 
-  def plot(title:String) = {
+  def plot(title:String): DAGView = {
 
     new DAGView(this, title)
 
