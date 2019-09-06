fd2d32770bd4adffe7cc20b4d82ad59c28b675c2
lucananni93 <lucananni93@gmail.com>
2019-09-06 12:11:36 +0200

    added isProtected

--- GMQL-Core/src/main/scala/it/polimi/genomics/core/DataStructures/IROperator.scala
+++ GMQL-Core/src/main/scala/it/polimi/genomics/core/DataStructures/IROperator.scala
@@ -14,11 +14,10 @@ abstract class IROperator extends Serializable with DAGNode[IROperator] {
   /** A list of the source datasets which are used by this operator */
   override def sources: Set[IRDataSet] = this.getDependencies.foldLeft(Set.empty[IRDataSet])((x, y) => x union y.sources)
 
-  def hasExecutedOn: Boolean = true //this.annotations.exists({case EXECUTED_ON(_) => true})
-  def getExecutedOn: GMQLInstance =
-    if (this.hasExecutedOn) this.annotations.collectFirst {
-      case EXECUTED_ON(instance) => instance
-    }.getOrElse(LOCAL_INSTANCE) else LOCAL_INSTANCE
+  def hasExecutedOn: Boolean = this.annotations.exists({case EXECUTED_ON(_) => true})
+  def getExecutedOn: GMQLInstance = this.annotations.collectFirst { case EXECUTED_ON(instance) => instance}.get
+
+  def isProtected: Boolean = this.annotations.exists({case PROTECTED => true})
 
   /** Optional intermediate result stored to speed up computations */
   var intermediateResult: Option[AnyRef] = None
@@ -117,4 +116,6 @@ trait ReadOperator {
   var paths: List[String]
   val loader: GMQLLoader[_, _, _, _]
   var dataset: IRDataSet
+
+  def isProtected: Boolean
 }
