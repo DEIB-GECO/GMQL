a7414c44f0444331763396d0c010bb20a6c8a1ec
lucananni93 <lucananni93@gmail.com>
2019-09-06 12:12:12 +0200

    added policies

--- GMQL-Federated/src/main/scala/it/polimi/genomics/federated/DistributionPolicy.scala
+++ GMQL-Federated/src/main/scala/it/polimi/genomics/federated/DistributionPolicy.scala
@@ -1,7 +1,7 @@
 package it.polimi.genomics.federated
 
 import it.polimi.genomics.core.DAG.OperatorDAG
-import it.polimi.genomics.core.DataStructures.{EXECUTED_ON, GMQLInstance, IROperator}
+import it.polimi.genomics.core.DataStructures.{EXECUTED_ON, GMQLInstance, IROperator, ReadOperator}
 import org.slf4j.LoggerFactory
 
 
@@ -13,6 +13,39 @@ import org.slf4j.LoggerFactory
   */
 trait DistributionPolicy {
   def assignLocations(dag: OperatorDAG) : OperatorDAG
+}
+
+class ProtectedDistributionPolicy extends DistributionPolicy {
+  override def assignLocations(dag: OperatorDAG): OperatorDAG = {
+
+    def getDependenciesConstraints(deps: List[IROperator]): List[GMQLInstance] = {
+      deps.map(decideLocation).filter(_.isDefined).map(_.get)
+    }
+
+    def decideLocation(op: IROperator): Option[GMQLInstance] = {
+      val loc = op match {
+        case operator: ReadOperator if operator.isProtected => Some(operator.getExecutedOn)
+        case _ => {
+          val depConstr: List[GMQLInstance] = if(op.hasDependencies)
+            getDependenciesConstraints(op.getDependencies) else List()
+          val constr = if(op.hasExecutedOn) op.getExecutedOn :: depConstr else depConstr
+          val uniqueNames = constr.map(_.name).distinct
+          if(uniqueNames.length > 1)
+            throw new GmqlFederatedException("Impossible to allocate resources:\n" +
+              s"Conflicting protection requirements (${uniqueNames.mkString(" VS ")})")
+          else if(uniqueNames.length == 1)
+            Some(constr.head)
+          else
+            None
+          }
+        }
+      if(!op.hasExecutedOn && loc.isDefined)
+        op.addAnnotation(EXECUTED_ON(loc.get))
+      loc
+    }
+    dag.roots.foreach(decideLocation)
+    dag
+  }
 }
 
 
@@ -59,3 +92,5 @@ class LocalityDistributionPolicy extends DistributionPolicy {
     dag
   }
 }
+
+
